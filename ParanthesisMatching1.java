4191675cb6afbea5454f0c7ed15bab5791f4f00a
Mohit singh <singhmohit9718@gmail.com>
2020-04-18 01:51:32 +0530

    Paranthesis Matching

---
+++ ParanthesisMatching1.java
@@ -0,0 +1,65 @@
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+}
+public class ParanthesisMatching1{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String");
+		String s=sc.next();
+		//System.out.println(isBalance(s));
+		if(isBalance(s))
+			System.out.println("Balanced");
+		else
+			System.out.println("Not Balanced");
+	}
+	public static boolean isBalance(String s){
+		LinkedList list=new LinkedList();
+		for(char ch:s.toCharArray()){
+			if ((ch=='(')||(ch=='{')||(ch=='['))
+				list.push(ch);
+			else{
+			if (((ch==')')&&(list.stackTop()==(int)'('))||((ch==']')&&(list.stackTop()==(int)'['))||((ch=='}')&&(list.stackTop()==(int)'{')))
+				list.pop();
+			else
+				return false;
+		}}
+		if (list.isEmpty())
+			return true;
+		else
+			return false;
+}}
+class LinkedList{
+	Node top;
+	public void push(int data){
+		Node node=new Node();
+		if (node==null){
+			System.out.println("OverFlow");
+		}else{
+			node.data=data;
+			node.next=top;
+			top=node;
+		}
+	}
+	public void pop(){
+		Node t;
+		if(top==null){
+			System.out.println("UnderFlow");
+		}else{
+			t=top;
+			top=top.next;
+			t=null;
+		}
+	}
+	public int stackTop(){
+		if (top==null)
+			return 0;
+		return top.data;
+	}
+	public boolean isEmpty(){
+		if (top==null)
+			return true;
+		return false;
+	}
+}	
